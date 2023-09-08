fdfa53c9b5eca0f16c7e84472b0aaed78d21fc18
CJL Fortunato <clemence.fortunato@gmail.com>
2023-09-08 15:06:18 +0200

    Connect database

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/db.js';
 import appsRouter from './applications/apps.routes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
 app.use('/api/apps', appsRouter);
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-
+import connectDB from './db/db.js';
 import appsRouter from './applications/apps.routes.js';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+connectDB();
 
 app.use('/api/apps', appsRouter);
 
