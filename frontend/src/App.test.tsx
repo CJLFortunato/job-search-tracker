753d1364d692c9a82d196b1391bd6af8584d6c46
Clémence Fortunato <c.fortunato@agence-wam.fr>
2023-10-17 12:49:50 +0200

    lint corrections

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './common/store';
+
+import { render } from '@testing-library/react';
+
 import App from './App';
+import { store } from './common/store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
-    </Provider>
+      <App />,
+    </Provider>,
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
