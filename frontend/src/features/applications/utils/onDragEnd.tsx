0bc589765ab34dad36f188aad2bd303c8abdda5d
Clémence Fortunato <c.fortunato@agence-wam.fr>
2023-10-30 16:30:53 +0100

    Keep working on dragndrop

--- frontend/src/features/applications/utils/onDragEnd.tsx
+++ frontend/src/features/applications/utils/onDragEnd.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+/* eslint-disable consistent-return */
+import React from 'react';
 
-import { useAppSelector } from "common/hooks";
+import { useAppSelector } from 'common/hooks';
 
-import AnswerStep from "../components/stepComponents/AnswerStep";
-import ApplyStep from "../components/stepComponents/ApplyStep";
-import FollowUpStep from "../components/stepComponents/FollowUpStep";
-import InterviewStep from "../components/stepComponents/InterviewStep";
+import AnswerStep from '../components/stepComponents/AnswerStep';
+import ApplyStep from '../components/stepComponents/ApplyStep';
+import FollowUpStep from '../components/stepComponents/FollowUpStep';
+import InterviewStep from '../components/stepComponents/InterviewStep';
 
 function useDrag() {
   const apps = useAppSelector((state) => state.appsReducer.apps);
@@ -14,22 +15,33 @@ function useDrag() {
     if (!destination || source.droppableId === destination.droppableId) return;
     const targetApp = apps.filter((app) => app._id === result.draggableId)[0];
     switch (destination.droppableId) {
-      case ("0"):
+      case ('0'):
         break;
-      case ("1"):
+      case ('1'):
         return (
           <ApplyStep application={targetApp} />
-        )
+        );
         break;
-      case ("2"):
+      case ('2'):
+        return (
+          <FollowUpStep application={targetApp} />
+        );
         break;
-      case ("3"):
+      case ('3'):
+        return (
+          <InterviewStep application={targetApp} />
+        );
         break;
-      case ("4"):
+      case ('4'):
+        return (
+          <AnswerStep application={targetApp} />
+        );
         break;
       default:
-        return;
+        break;
     }
-  }
+  };
+
+  return { onDragEnd };
 }
 export default useDrag;
