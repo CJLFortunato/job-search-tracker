72e0774e5ade95093f7ccb562a5ee9fd79b02826
Clémence Fortunato <c.fortunato@agence-wam.fr>
2023-10-30 16:31:15 +0100

    Add footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Footer from 'components/Footer';
 import Header from './components/Header';
 
 import './normalize.css';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      This is a footer. I have no idea what I&apos;m going to put in here.
+      Github/portfolio link maybe.
+    </footer>
+  );
+}
+
+export default Footer;
