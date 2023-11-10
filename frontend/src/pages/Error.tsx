54f35d932f655ad63cc4053eea4b6348f9d2fc8a
Clémence Fortunato <c.fortunato@agence-wam.fr>
2023-11-10 09:59:30 +0100

    Style error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
     <div className="page error">
-      <h1>La page que vous cherchez n&apos;existe pas</h1>
+      <h1>404: Page non trouvée</h1>
+      <p>La page que vous cherchez n&apos;existe pas</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
