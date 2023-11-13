5094d766ced67125e1160b4c32f67e35c2b46035
CJL Fortunato <clemence.fortunato@gmail.com>
2023-11-13 10:15:26 +0100

    Sort selected tags first

--- frontend/src/features/tags/components/SelectTags.tsx
+++ frontend/src/features/tags/components/SelectTags.tsx
@@ -4,10 +4,16 @@ import { SelectTagsProps } from '../types';
 
 function SelectTags(props: SelectTagsProps) {
   const { tags, selectedTags, handleSelect } = props;
+  const sortedTags = [...tags].sort((a, b) => {
+    if (selectedTags.includes(a._id) && !selectedTags.includes(b._id)) return -1;
+    if (!selectedTags.includes(a._id) && selectedTags.includes(b._id)) return 1;
+    return 0;
+  });
+
   return (
     <div className="select-tags">
       {
-        tags.map((tag) => (
+        sortedTags.map((tag) => (
           <button
             type="button"
             className={selectedTags.includes(tag._id) ? 'tag-btn-active' : 'tag-btn'}
